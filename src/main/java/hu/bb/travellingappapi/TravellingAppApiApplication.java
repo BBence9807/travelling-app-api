226deef6442a07113b20b8a737e9b76f051ea908
BBence0798 <bbebe07@gmail.com>
2023-11-17 18:23:05 +0100

    Initial commit

---
+++ src/main/java/hu/bb/travellingappapi/TravellingAppApiApplication.java
@@ -0,0 +1,13 @@
+package hu.bb.travellingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingAppApiApplication.class, args);
+	}
+
+}
